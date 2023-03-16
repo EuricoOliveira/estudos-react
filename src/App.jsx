6e8c37efbd27401663a7062a54d884580d6cccb7
https://github.com/EuricoOliveira/html.git <https://github.com/EuricoOliveira/html.git>
2023-03-16 09:55:23 -0300

    Aula React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { ChecarNumero } from "./components/ChecarNumero/ChecarNumero";
+import { ListaAlunos } from "./components/ListaAlunos/ListaAlunos";
+import { ListaCompras } from "./components/ListaCompras/ListaCompras";
+import { ListaFuncionarios } from "./components/ListaFuncionarios/ListaFuncionarios";
+import { NavBar } from "./components/NavBar/NavBar";
+import { ProductList } from "./components/ProductList/ProductList";
+
+export function App() {
+  return (
+    <>
+      <ProductList />
+    </>
+  );
+}
